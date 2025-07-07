857fdee7e2814c68421477849d4c39be71a76fa0
David Inhof <yrwqid@gmail.com>
2025-07-08 00:31:16 +0200

    delete body view, run as background app

--- bluhr/bluhrApp.swift
+++ bluhr/bluhrApp.swift
@@ -12,29 +12,8 @@ struct bluhrApp: App {
     @NSApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
     
     var body: some Scene {
-        WindowGroup {
-            ContentView()
+        Settings {
+            EmptyView()
         }
-        .windowStyle(.hiddenTitleBar)
-        .windowResizability(.contentSize)
-    }
-}
-
-struct ContentView: View {
-    var body: some View {
-        VStack {
-            Text("Bluhr - Wallpaper Blur Wrapper")
-                .font(.title2)
-                .foregroundColor(.white)
-            Text("Your wallpaper is now blurred in the background")
-                .font(.caption)
-                .foregroundColor(.gray)
-            Text("Check your desktop for the blur overlay")
-                .font(.caption)
-                .foregroundColor(.blue)
-        }
-        .frame(width: 300, height: 150)
-        .background(.ultraThinMaterial)
-        .cornerRadius(12)
     }
 }

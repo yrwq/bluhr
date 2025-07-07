1a8cc86aba00b425a6657ffb21e379ee5feecff2
David Inhof <yrwqid@gmail.com>
2025-07-07 23:57:54 +0200

    initial

---
+++ bluhr/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  bluhr
+//
+//  Created by yrwq on 2025. 07. 07..
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
---
+++ bluhr/bluhrApp.swift
@@ -0,0 +1,17 @@
+//
+//  bluhrApp.swift
+//  bluhr
+//
+//  Created by yrwq on 2025. 07. 07..
+//
+
+import SwiftUI
+
+@main
+struct bluhrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
